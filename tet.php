9df20ca2f8b9d78e6f0c1083793d1d33c340a836
sexBookkan <79981885+sexBookkan@users.noreply.github.com>
2021-09-07 20:07:48 +0530

    tet.html

--- tet.php
+++ tet.php
@@ -1,10 +1,1 @@
-<!DOCTYPE html>
-<html>
-   <head>
-      <title>HTML Meta Tag</title>
-      <meta http-equiv = "refresh" content = "2; url = lustadda-stories.blogspot.com" />
-   </head>
-   <body>
-      <p>Hello HTML5!</p>
-   </body>
-</html>
+<meta http-equiv="refresh" content="0; URL=lustadda-stories.blogspot.com" />
